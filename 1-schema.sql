d5b384920938bcd8ca2886f6c00c0b83a43da871
loicortola <loic.ortola@free.fr>
2013-09-25 14:30:20 +0200

    Update 1-schema.sql
    Bugfix in sql script

--- 1-schema.sql
+++ 1-schema.sql
@@ -26,10 +26,10 @@ create index ix_computer_company_1 on computer (company_id);
 #-----------------------------------
 #USER RIGHTS MANAGEMENT
 #-----------------------------------
-CREATE USER 'jee-computer-database'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'jee-cdb'@'localhost' IDENTIFIED BY 'password';
 
-GRANT ALL PRIVILEGES ON `computer`.* TO 'jee-computer-database'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `computer`.* TO 'jee-cdb'@'localhost' WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES ON `company`.* TO 'jee-computer-database'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `company`.* TO 'jee-cdb'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
