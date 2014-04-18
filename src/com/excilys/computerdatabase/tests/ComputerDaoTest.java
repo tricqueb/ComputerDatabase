9257e35f7d30e2dbdc8eec6ec1fa3da3da05b17e
TimotheeRicquebourg <tricquebourg@excilys.com>
2014-04-18 11:29:23 +0200

    rid off id

--- src/com/excilys/computerdatabase/tests/ComputerDaoTest.java
+++ src/com/excilys/computerdatabase/tests/ComputerDaoTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.excilys.computerdatabase.dao.ComputerDao;
 import com.excilys.computerdatabase.models.Computer;
-import com.excilys.computerdatabaseDao.ComputerDao;
 
 @RunWith(JUnit4.class)
 public class ComputerDaoTest {
@@ -17,7 +17,6 @@ public class ComputerDaoTest {
 		ComputerDao cd = ComputerDao.getInstance();
 		Computer computer = new Computer();
 
-		computer.setId(0);
 		computer.setName("poil");
 		computer.setIntroduced(new Date(0));
 		computer.setDiscontinued(new Date(0));
